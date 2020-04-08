754ed9b7fe6838cc546a354b44ca00ee6b3b3442
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-08 09:15:49 -0500

    I have met MVP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import Logo from "./download.png"
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      git: []
+      user: {},
+      followers: [],
+      login: "",
+      img: ""
     };
   }
 
   componentDidMount() {
-    axios.get("https://api.github.com/users/dawsonhammdev").then(response => {
-      this.setState({
-        git: response.data.message,
-      });
+    axios.get("https://api.github.com/users/dawsonhammdev")
+    .then(response => {
+      console.log(response.data)
+       this.setState({
+         user: response.data,
+         login: response.data.login,
+         img: response.data.avatar_url,
+       });
     });
+
+    axios.get("https://api.github.com/users/dawsonhammdev/followers")
+    .then(peeps => {
+      console.log(peeps.data)
+      this.setState({
+        followers: peeps.data.length
+      })
+    })
+  }
+
+  render() {
+    return (
+      
+      <div className="App">
+        <img className ="logo" src={Logo} alt="website logo"/>
+
+      <div className="user">
+
+    <h2>User Card</h2>
+    <h3>{this.state.login}</h3>
+    <img className="head" src={this.state.img} alt="Man" />
+      <h3>Followers: {this.state.followers}</h3>
+
+      </div>
+      {console.log(this.state.user)}
+      </div>
+      
+    )
   }
 
 }
